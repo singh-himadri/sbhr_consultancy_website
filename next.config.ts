ef42c361d5ede64f50234ad2de4602df70b3340a
jai2256 <singhhimadri727@gmail.com>
2026-08-29 19:29:26 +0530

    Updated Next Config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  output: "export"
+  output: "export",
+   images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
